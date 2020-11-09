6f89b4e29c81db50338848b2b5485a4ec01940c5
Mayrinika <mashuliy_u@mail.ru>
2020-11-09 20:55:09 +0500

    layout modal window

--- createButton.js
+++ createButton.js
@@ -3,6 +3,7 @@ import defaultSettings from './defaultSettings.js';
 function createButton() {
     const button = document.createElement('botton');
     button.classList.add(defaultSettings.classes.button);
+    button.innerHTML = '🦝';
     return button;
 }
 
