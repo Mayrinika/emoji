79e53c70d5ada8f61d32ff573b24dbec13973429
Mayrinika <mashuliy_u@mail.ru>
2020-11-10 19:29:48 +0500

    fix bugs

--- createButton.js
+++ createButton.js
@@ -1,7 +1,7 @@
 import defaultSettings from './defaultSettings.js';
 
 function createButton() {
-    const button = document.createElement('botton');
+    const button = document.createElement('button');
     button.classList.add(defaultSettings.classes.button);
     button.innerHTML = '🦝';
     return button;
