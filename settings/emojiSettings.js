3880bb29b518e55b482f5a1fa80fb43c85a7fa48
Mayrinika <mashuliy_u@mail.ru>
2020-12-06 17:50:55 +0500

    fix parsing

--- settings/emojiSettings.js
+++ settings/emojiSettings.js
@@ -6,41 +6,37 @@ function parsing() {
         title: '',
         emojiList: [],
     };
-    let numberOfSubtotal = 0;
-    const strings = emojiData.split('\n');
+    const strings = emojiData.toLowerCase().split('\n');
     for (const string of strings) {
-        if (string === '') continue;
-        if (string.includes('unqualified')) continue;
-        if (string.includes('component')) continue;
-        if (string.includes('Component')) continue;
+        if (string === '' || string.includes('unqualified') || string.includes('component')) continue;
         if (string[0] === '#') {
             if (string.includes('subgroup')) continue;
             else if (string.includes('group')) {
                 category.title = string.slice(9);
-            } else if (string.includes('subtotal')) {
-                numberOfSubtotal++;
-                if (numberOfSubtotal === 2) {
-                    category.icon = category.emojiList[0].emotion;
-                    result.push(category);
-                    category = {
-                        title: '',
-                        emojiList: [],
-                    };
-                    numberOfSubtotal = 0;
-                }
-            } else continue;
+            } else if (string.includes('modifiers')) {
+                category.icon = category.emojiList[0].emotion;
+                result.push(category);
+                category = {
+                    title: '',
+                    emojiList: [],
+                };
+            }
         } else {
-            const partsOfString = string.split(';');
-            const emojiIndex = partsOfString[1].indexOf('#') + 2;
-            const firstSpaceAfterEmoji = partsOfString[1].indexOf(' ', emojiIndex);
-            const emotion = partsOfString[1].slice(emojiIndex, firstSpaceAfterEmoji);
-            category.emojiList.push({
-                emotion,
-                keywords: partsOfString[1].slice(emojiIndex + emotion.length + 1),
-            });
+            category.emojiList.push(parseEmojiList(string));
         }
     }
     return result;
+}
+
+function parseEmojiList(string) {
+    const partsOfString = string
+        .split(/[;|#]/)
+        .map(str => str.trim());
+    const firstSpaceAfterEmoji = partsOfString[2].indexOf(' ');
+    return {
+        emotion: partsOfString[2].slice(0, firstSpaceAfterEmoji),
+        keywords: partsOfString[2].slice(firstSpaceAfterEmoji + 1),
+    }
 }
 
 export default parsing();
