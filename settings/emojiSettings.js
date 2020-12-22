352737034bf1ec8c268a3816614adf5210ea66a4
Mayrinika <mashuliy_u@mail.ru>
2020-12-22 22:26:01 +0500

    refactoring

--- settings/emojiSettings.js
+++ settings/emojiSettings.js
@@ -1,21 +1,22 @@
 import emojiData from './emojiData.js';
 
+const strings = emojiData
+    .toLowerCase()
+    .split('\n')
+    .filter(str =>
+        str !== '' &&
+        !str.includes('minimally') &&
+        !str.includes('unqualified') &&
+        !str.includes('component') &&
+        !str.includes('subgroup')
+    );
+
 function parsing() {
     const result = [];
     let category = {
         title: '',
         emojiList: [],
     };
-    const strings = emojiData
-        .toLowerCase()
-        .split('\n')
-        .filter(str =>
-            str !== '' &&
-            !str.includes('minimally') &&
-            !str.includes('unqualified') &&
-            !str.includes('component') &&
-            !str.includes('subgroup')
-        );
     for (let i = 0; i < strings.length;) {
         const string = strings[i];
         if (string[0] === '#') {
@@ -32,7 +33,7 @@ function parsing() {
             }
             i++;
         } else {
-            const emoji = getEmoji(strings, i);
+            const emoji = getEmoji(i);
             category.emojiList.push(emoji);
             if (emoji.emotions['light']) i += 6;
             else i++;
@@ -41,7 +42,7 @@ function parsing() {
     return result;
 }
 
-function getEmoji(strings, i) {
+function getEmoji(i) {
     let emotions = {};
     const [code, qualifications, emojiData] = strings[i]
         .split(/[;|#]/)
@@ -49,14 +50,14 @@ function getEmoji(strings, i) {
     const firstSpaceAfterEmoji = emojiData.indexOf(' ');
     const keywords = emojiData.slice(firstSpaceAfterEmoji + 1);
     if (strings[i + 1].includes(' tone')) {
-        emotions = getEmotions(strings, i);
+        emotions = getEmotions(i);
     } else {
         emotions['gold'] = emojiData.slice(0, firstSpaceAfterEmoji);
     }
     return {emotions, keywords};
 }
 
-function getEmotions(strings, i) {
+function getEmotions(i) {
     const colors = ['gold', 'light', 'medium-light', 'medium', 'medium-dark', 'dark'];
     const emotions = {};
     for (let j = i, c = 0; j < i + 6, c < colors.length; j++, c++) {
